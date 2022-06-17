8219bfbd2c04e02d8f1d79c65f873fe4a176af4a
Ross Nesbit <rnesbit@nexus-network.cloud>
2022-06-17 07:54:51 -0500

    Add LayoutSyncComponent for LayoutComponent.

---
+++ src/BlazorUISyncManager/UILayoutSyncComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusNetworkCloud.BlazorUISyncManager
+{
+    public class UILayoutSyncComponent : LayoutComponentBase, IObserver<UISyncEvent>, IDisposable
+    {
+        [Inject]
+        public IUISyncManager? UISyncManager { get; set; }
+
+        private IDisposable? uiSyncDisposer;
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                uiSyncDisposer = UISyncManager?.Subscribe(this);
+            }
+
+            base.OnAfterRender(firstRender);
+        }
+
+        public void Dispose() => uiSyncDisposer?.Dispose();
+
+        public void OnCompleted() => throw new NotImplementedException();
+
+        public void OnError(Exception error) => throw new NotImplementedException();
+
+        public async void OnNext(UISyncEvent value) => await InvokeAsync(StateHasChanged);
+    }
+}
